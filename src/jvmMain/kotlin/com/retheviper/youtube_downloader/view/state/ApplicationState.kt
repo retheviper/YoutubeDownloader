78f6e30c27c26fde82063d250998a097b029590c
Youngbin Kim <retheviper@gmail.com>
2023-04-25 19:16:45 +0900

    [Fix] Unresolved reference: rememberApplicationState (#2)

---
+++ src/jvmMain/kotlin/com/retheviper/youtube_downloader/view/state/ApplicationState.kt
@@ -0,0 +1,18 @@
+package com.retheviper.youtube_downloader.view.state
+
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
+
+class ApplicationState(
+    val downloadOption: DownloadOptionState,
+    val userInput: UserInputState
+)
+
+@Composable
+fun rememberApplicationState(
+    downloadOptionState: DownloadOptionState = rememberDownloadOptionState(),
+    userInputState: UserInputState = rememberUserInputState()
+): ApplicationState =
+    remember(downloadOptionState, userInputState) {
+        ApplicationState(downloadOptionState, userInputState)
+    }
